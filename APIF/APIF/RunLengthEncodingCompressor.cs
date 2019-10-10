efca5a8d96e2d1baa690c2645f30254c18093ee1
BuildTools <unconfigured@null.spigotmc.org>
2019-10-10 18:33:21 +0200

    Made start on RLE Compressor

--- APIF/APIF/RunLengthEncodingCompressor.cs
+++ APIF/APIF/RunLengthEncodingCompressor.cs
@@ -9,9 +9,51 @@ namespace APIF
 {
     class RunLengthEncodingCompressor
     {
+        ApifEncoder.AccessibleBitmap bmp;                       // Variable for storing the bitmap
+
+        //Constructor to assign bitmap on creation of the compressor
         public RunLengthEncodingCompressor(ApifEncoder.AccessibleBitmap input)
         {
+            bmp = input;   
+        }
 
+        // This function will compress te bitmap and return a new AccessibleBitmap
+        public ApifEncoder.AccessibleBitmap RLECompress()
+        {
+            Color[,] colors = new Color[bmp.width, bmp.height];         // 2D color array for setting the bytes to colors for easy comparison
+
+            // Iterate through all the pixels of the bitmap, get their ARGB values and store them as colors in the 2D color array
+            for (int y = 0; y < bmp.height; y++)
+            {
+                for (int x = 0; x < bmp.width; x++)
+                {
+                    byte[] pixel = bmp.GetPixel(x, y);
+                    Color c;
+                    if (pixel.Length % 3 == 0)
+                    {
+                        // Has no alpha channel
+                        if (pixel.Length == 3)
+                            c = Color.FromArgb(pixel[0], pixel[1], pixel[2]);
+                        else
+                            c = Color.FromArgb(pixel[0] * 256 + pixel[1], pixel[2] * 256 + pixel[3], pixel[4] * 256 + pixel[5]);
+                    }
+                    else
+                    {
+                        // Has alpha channel
+                        if (pixel.Length == 4)
+                            c = Color.FromArgb(pixel[0], pixel[1], pixel[2], pixel[3]);
+                        else
+                            c = Color.FromArgb(pixel[0] * 256 + pixel[1], pixel[2] * 256 + pixel[3], pixel[4] * 256 + pixel[5], pixel[6] * 256 + pixel[7]);
+                    }
+
+                    colors[x, y] = c;
+                }
+            }
+
+            // Compare per row the pixels next to eachother and compress
+            // Turn all the color values back to byte arrays and save them in a AccessibleBitmap
+
+            return null;
         }
     }
 }
