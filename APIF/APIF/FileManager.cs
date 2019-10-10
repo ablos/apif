6fe46cbff88d2d2363cf0d6600b258deafcfa3f2
BuildTools <unconfigured@null.spigotmc.org>
2019-10-10 17:35:07 +0200

    Added filemanager for saving and loading binary save files for saving objects

---
+++ APIF/APIF/FileManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+/*
+ * Try saving in binary
+ * Try saving in XML
+ * Try saving in JSON
+ */
+
+namespace APIF
+{
+    public static class FileManager
+    {
+        public static void SaveFileBinary<T>(string path, T objectToSave, bool append = false)
+        {
+            using (Stream stream = File.Open(path, FileMode.Open))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, objectToSave);
+            }
+        }
+
+        public static T LoadFileBinary<T>(string path)
+        {
+            using (Stream stream = File.Open(path, FileMode.Open))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
