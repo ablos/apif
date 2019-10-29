42d74f8f2e3693f57426257a937040c49b9e3496
BuildTools <unconfigured@null.spigotmc.org>
2019-10-29 20:55:51 +0100

    Added explanation to the RLE Compressor class.

--- APIF/APIF/RunLengthEncodingCompressor.cs
+++ APIF/APIF/RunLengthEncodingCompressor.cs
@@ -8,6 +8,13 @@ using static APIF.ApifEncoder;
 
 namespace APIF
 {
+    /*
+     * - Create an instance of this class to compress with Run Length Encoding (RLE).
+     * - To compress, use the CompressHorizontal or CompressVertical function (sizes of these are to be compared later).
+     * - To decompress, just use the Decompress function, this function will determine if the picture was vertically or horizontally compressed and
+     *   will decompress it into a AccessibleBitmap.
+     */
+
     class RunLengthEncodingCompressor
     {
         // Create a new list to add bytes and will be returned after
