8acd309c1d475ad8b1ab542077016cafc5e71b80
BuildTools <unconfigured@null.spigotmc.org>
2019-11-26 21:18:39 +0100

    Added new RLE class for compressing on bit level instead of byte level

---
+++ APIF/APIF/RLEBitCompressor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using static APIF.ApifEncoder;
+using System.Linq;
+
+namespace APIF
+{
+    /*
+     * - Create an instance of this class to compress with Run Length Encoding (RLE).
+     * - To compress, use the CompressHorizontal or CompressVertical function (sizes of these are to be compared later).
+     * - To decompress, just use the Decompress function, this function will determine if the picture was vertically or horizontally compressed and
+     *   will decompress it into a AccessibleBitmap.
+     */
+
+    static class RLEBitCompressor
+    {
+
+    }
+}
--- APIF/APIF/RunLengthEncodingCompressor.cs
+++ APIF/APIF/RunLengthEncodingCompressor.cs
@@ -3,11 +3,11 @@ using static APIF.ApifEncoder;
 using System.Linq;
 // Delete these:
 using System;
-using System.IO;
 
 namespace APIF
 {
     /*
+     * !!! WARNING: THIS CLASS IS OBSOLETE AND OLD, PLEASE USE RLEBitCompressor !!!
      * - Create an instance of this class to compress with Run Length Encoding (RLE).
      * - To compress, use the CompressHorizontal or CompressVertical function (sizes of these are to be compared later).
      * - To decompress, just use the Decompress function, this function will determine if the picture was vertically or horizontally compressed and
